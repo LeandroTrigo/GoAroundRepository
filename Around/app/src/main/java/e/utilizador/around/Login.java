a61c213286b8512c0b35997c0885436d96cf1f24
Leandro <leocostrigo@gmail.com>
2020-02-18 18:23:39 +0000

    Commit

---
+++ Around/app/src/main/java/e/utilizador/around/Login.java
@@ -0,0 +1,31 @@
+package e.utilizador.around;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Login extends AppCompatActivity {
+
+    Button login;
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_activity);
+
+        login = findViewById(R.id.login);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Login.this, DoLogin.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+}
