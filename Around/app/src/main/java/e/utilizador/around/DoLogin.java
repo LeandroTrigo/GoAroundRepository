d6f86a6ee98c04efc961708342b3ff8fe0e1f0c4
Leandro <leocostrigo@gmail.com>
2020-02-19 20:11:12 +0000

    Update Registo

--- Around/app/src/main/java/e/utilizador/around/DoLogin.java
+++ Around/app/src/main/java/e/utilizador/around/DoLogin.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,12 +13,14 @@ import e.utilizador.around.R;
 public class DoLogin extends AppCompatActivity {
 
     Button loggedin;
+    TextView registar;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity_2);
 
         loggedin = findViewById(R.id.button_login);
+        registar = findViewById(R.id.nova_conta);
 
 
         loggedin.setOnClickListener(new View.OnClickListener() {
@@ -27,7 +30,17 @@ public class DoLogin extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        registar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DoLogin.this, Registo.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
 
 
 }
